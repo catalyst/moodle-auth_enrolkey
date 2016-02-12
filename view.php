72cbe4805155c5a91c53fa12bd3d752063d39d1f
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2016-02-12 13:32:19 +1100

    Multiple changes to view.php
    Updated copyright and document strings.
    
    Prints output with the enrolled courses at signup.

--- view.php
+++ view.php
@@ -15,25 +15,67 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Login page hook to notify user of courses enrolled via tokens.
+ * Post signup page to notify user of courses enrolled via tokens.
  *
  * @package    auth_token
- * @copyright  2016 Nicholas Hoobin
+ * @copyright  2016 Nicholas Hoobin (nicholashoobin@catalyst-au.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-//defined('MOODLE_INTERNAL') || die;
+defined('MOODLE_INTERNAL') || die;
+
 require_once('../../config.php');
 
 $PAGE->set_url(new moodle_url('/admin/token/view.php'));
 
-
-$PAGE->set_pagelayout('print');
-
 $PAGE->set_course($SITE);
-$PAGE->set_title('title');
-$PAGE->set_heading('heading');
+$PAGE->set_title(get_string('pluginname', 'auth_token'));
+$PAGE->set_heading(get_string('auth_tokensignup_view', 'auth_token'));
 
 echo $OUTPUT->header();
+
+if (isset($SESSION->auth_token)) {
+    $authtoken = $SESSION->auth_token;
+
+    $enrolplugins = $DB->get_records('enrol', array('enrol' => 'self', 'password' => $authtoken));
+
+    foreach ($enrolplugins as $plugin) {
+        $course = $DB->get_record('course', array('id' => $plugin->courseid));
+
+        $coursecontext = context_course::instance($plugin->courseid);
+        $rolenames = role_get_names($coursecontext, ROLENAME_ALIAS, true);
+
+        $data = new stdClass();
+        $data->course        = $course->fullname;
+        $data->enrolinstance = $plugin->name;
+        $data->role          = $rolenames[$plugin->roleid];
+        $data->startdate     = date('Y-m-d H:i', $plugin->enrolstartdate);
+        $data->enddate       = date('Y-m-d H:i', $plugin->enrolenddate);
+
+        if ($plugin->enrolstartdate > 0 && $plugin->enrolenddate > 0) {
+            // The course had both a start and end date.
+            $successoutput = get_string('auth_tokensignup_view_message_basic_dates', 'auth_token', $data);
+
+        } else if ($plugin->enrolstartdate > 0 && $plugin->enrolenddate == 0) {
+            // The course only has a start date set.
+            $successoutput = get_string('auth_tokensignup_view_message_basic_dates_startonly', 'auth_token', $data);
+
+        } else if ($plugin->enrolstartdate == 0 && $plugin->enrolenddate > 0) {
+            // The course only has a start date set.
+            $successoutput = get_string('auth_tokensignup_view_message_basic_dates_endonly', 'auth_token', $data);
+
+        } else {
+            // The course has no date restrictions.
+            $successoutput = get_string('auth_tokensignup_view_message_basic', 'auth_token', $data);
+
+        }
+
+        echo $OUTPUT->notification($successoutput, 'notifysuccess');
+    }
+}
+
+echo $OUTPUT->continue_button(new moodle_url('/index.php'));
+
 echo $OUTPUT->footer();
 
+unset($SESSION->auth_token);
