5abbe8f499314b6ce3cbe785f0a0eb3e85dc56af
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2016-02-15 11:15:35 +1100

    Changed version.php $plugin->requires to Moodle 2.7

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2016020800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015050500;        // Requires this Moodle version.
+$plugin->requires  = 2014051200;        // Requires this Moodle version.
 $plugin->component = 'auth_token';      // Full name of the plugin (used for diagnostics).
