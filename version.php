d6cc310ad7989554dbd4aeb1386aee94bbdbf90a
Anupama Dharmajan <anupamadharmajan@catalyst-au.net>
2021-12-15 15:35:51 +1100

    Add reusable workflows

--- version.php
+++ version.php
@@ -24,8 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2021021700.03;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2021021700.03;        // Match release exactly to version.
+$plugin->version   = 2022010700;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2022010700;        // Match release exactly to version.
 $plugin->requires  = 2014051200;        // Requires this Moodle version. (2.7+)
 $plugin->component = 'auth_enrolkey';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
+$plugin->supported = [35, 311];            // A range of branch numbers of supported moodle versions.
